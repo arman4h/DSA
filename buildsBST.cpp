dbc81aa66b7e490a285462d1aab064df65cf8815
Arman Hossain <hossain4arman@gmail.com>
2026-01-26 23:21:38 +0600

    buildtheBST

---
+++ buildsBST.cpp
@@ -0,0 +1,64 @@
+// Insert Nodes in BST 
+
+#include<iostream> 
+#include<vector> 
+using namespace std ; 
+
+class Node{
+public: 
+    int data ; 
+    Node* left ; 
+    Node* right ; 
+
+    Node(int val){
+        data = val ; 
+        left = right = NULL ; 
+    }
+
+}; 
+
+Node* insert(Node* root, int val) {
+     if(root == NULL) {
+        return new Node(val) ; 
+     }
+
+     if(val < root-> data) {
+        root ->  left = insert(root->left, val) ; 
+     } else {
+        root -> right = insert(root->right, val) ;
+     }
+
+     return root ;
+}
+
+void inorder(Node* root) {
+    if(root == NULL ) {
+        return ;
+    }
+
+    inorder(root -> left) ; 
+    cout << root -> data << " " ;
+    inorder(root-> right) ;
+}
+
+Node* buildBST(vector<int> arr){
+    Node* root = NULL ; 
+
+    for(int val : arr){
+        root = insert(root, val) ; 
+    }
+
+    return root ; 
+}
+
+
+int main( ){
+    vector<int> bst = {3,2,1,4,5,6} ; 
+
+    Node* root = buildBST(bst); 
+    inorder(root) ; 
+    cout << endl ; 
+
+
+    return 0 ;
+}
